6e545664d29d93f1d7c27f7825962b387cd40972
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-02 12:49:37 +0900

    Initial Commit

---
+++ HeaderAnimation/HeaderAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderAnimationApp.swift
+//  HeaderAnimation
+//
+//  Created by Seungchul Ha on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct HeaderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
